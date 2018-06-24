fea8f78e6654083d5f254ac653a6effacd1d8ca1
Kaluchii <mr.kaluchii@mail.ru>
2018-06-25 00:13:25 +0600

    Changes

--- resources/views/front/prices/prices.blade.php
+++ resources/views/front/prices/prices.blade.php
@@ -10,9 +10,16 @@
     <div class="prices">
         <h1 class="prices__title">Цены на балконы</h1>
         <p class="prices__subtitle">Цены указаны только за пластиковые изделия (Без М/С, подоконников, отливов, монтажа и доставки).</p>
-        <div class="prices__varieties">
-            <p class="prices__varieties-title">Г - Образные балконы</p>
-            .prices__
+        <div class="prices__categories">
+            <p class="prices__categories-title">Г - Образные балконы</p>
+            <div class="prices__variant">
+                <div class="prices__img-wrap">
+                    <img src="" alt="" class="prices__img">
+                </div>
+                <div class="prices__text-wrap">
+                    .prices__row.prices__row--title
+                </div>
+            </div>
         </div>
         <div class="prices__text text-block"></div>
     </div>
