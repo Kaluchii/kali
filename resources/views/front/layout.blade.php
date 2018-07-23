bdcb5e22c809032b9f6748c4c90f9a02b125ba6b
kaluchii <mr.kaluchii@mail.ru>
2018-07-23 08:17:20 +0600

    Close for indexing

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+    @if($app->environment('local'))
+        <meta name="robots" content="noindex, nofollow" />
+    @endif
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
